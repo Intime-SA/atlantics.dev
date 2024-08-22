5f9727558e20f1d4b5bad1905dc8e6dd9d5c4c65
Intime-SA <invertimesa@gmail.com>
2024-08-22 15:38:07 -0300

    cambios rama antes de pepo

--- src/components/pages/services-pages/Cost.jsx
+++ src/components/pages/services-pages/Cost.jsx
@@ -1,20 +1,14 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import NewNavbar from '../../layout/Newnavbar';
-import { styled } from '@mui/system';
-import './costos.css';
-import MainIdeas from './MainIdeas';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import NewNavbar from "../../layout/Newnavbar";
+import { styled } from "@mui/system";
+import "./costos.css";
+import MainIdeas from "./MainIdeas";
 
-
-const ImageContainer = styled('div')({
-  display: 'flex',
-  justifyContent: 'center',
-  margin: '20px 0',
-});
-
-const ImageContent = styled('img')({
-  width: '40vw',
-  height: 'auto',
+const ImageContent = styled("img")({
+  width: "90%",
+  maxWidth: "500px",
+  height: "auto",
 });
 
 const Cost = () => {
@@ -27,33 +21,54 @@ const Cost = () => {
   useEffect(() => {
     AOS.init({
       duration: 2000,
-      easing: 'ease-out-cubic',
+      easing: "ease-out-cubic",
     });
   }, []);
   return (
     <div className="container-principal">
       <NewNavbar />
-      
+
       <section className="cost-container">
-      <div className='cost-main'>
-          <h1 className="cost-title" data-aos="fade-down">Costos y Servicios</h1>
-          <p className="cost-paragraph" 
+        <div className="cost-main">
+          <h1 className="cost-title" data-aos="fade-down">
+            Costos y Servicios
+          </h1>
+          <p
+            className="cost-paragraph"
             data-aos="flip-left"
             data-aos-easing="ease-out-cubic"
-            data-aos-duration="2000" >
-          En <strong style={{fontFamily:"'Montserrat', sans-serif"}}>Atlantics.dev</strong>, desarrollamos software pensado para optimizar tu negocio de eCommerce. Nuestras soluciones te permiten avanzar sin interrupciones ni costos inesperados, manteniéndote enfocado en el crecimiento.
+            data-aos-duration="2000"
+            style={{
+              textAlign: "center",
+              fontWeight: "100",
+              fontFamily: "'Montserrat', sans-serif",
+            }}
+          >
+            En{" "}
+            <strong style={{ fontFamily: "'Montserrat', sans-serif" }}>
+              Atlantics.dev
+            </strong>
+            , desarrollamos software pensado para optimizar tu negocio de
+            eCommerce. Nuestras soluciones te permiten avanzar sin
+            interrupciones ni costos inesperados, manteniéndote enfocado en el
+            crecimiento.
           </p>
         </div>
 
-        <ImageContainer>
-          <ImageContent src="/public/Grafico-ingresos.png" alt="Grafico de ingresos" />
-        </ImageContainer>
-        <div style={{
-          width:'50%',
-          display:'flex',
-          justifyContent:'center',
-        }}>
-        <MainIdeas />
+        <ImageContent
+          src="https://firebasestorage.googleapis.com/v0/b/mayoristakaurymdp.appspot.com/o/Grafico-ingresos.png?alt=media&token=cfb685a2-83e1-4560-b8a0-6fe76d6c6743"
+          alt="Grafico de ingresos"
+        />
+
+        <div
+          style={{
+            width: "100%",
+            maxWidth: "800px",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <MainIdeas />
         </div>
         <button
           className="contact-button"
@@ -64,6 +79,6 @@ const Cost = () => {
       </section>
     </div>
   );
-}
+};
 
 export default Cost;
--- src/components/pages/services-pages/MainIdeas.jsx
+++ src/components/pages/services-pages/MainIdeas.jsx
@@ -1,127 +1,168 @@
-import React, { useEffect } from 'react';
-import { Divider, Typography, Box } from '@mui/material';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import './costos.css';
+import React, { useEffect } from "react";
+import { Divider, Typography, Box } from "@mui/material";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./costos.css";
 
 const MainIdeas = () => {
-    useEffect(() => {
-        AOS.init({
-        duration: 2000, // Configura la duración global de las animaciones
-        easing: 'ease-out-cubic', // Configura el easing global de las animaciones
-        });
-    }, []);
+  useEffect(() => {
+    AOS.init({
+      duration: 2000, // Configura la duración global de las animaciones
+      easing: "ease-out-cubic", // Configura el easing global de las animaciones
+    });
+  }, []);
   return (
-<Box sx={{ padding: '20px' }}>
+    <Box sx={{ padding: "20px" }}>
       {/* Sección 1 */}
-      <Box 
-        component="h6" 
+      <Box
+        component="h6"
         sx={{
-            fontWeight: 'bold', mb: 1,
-            fontFamily: "'Raleway', sans-serif",
-            fontSize: '1.3rem'
-        }} 
+          fontWeight: "bold",
+          mb: 1,
+          fontFamily: "'Raleway', sans-serif",
+          fontSize: "1.3rem",
+          margin: "1rem",
+          textAlign: "left",
+          marginLeft: "0rem",
+        }}
         data-aos="zoom-in-left" // Animación para esta sección
       >
         Costo Inicial Bajo
       </Box>
-      <Box 
-        component="p" 
-        sx={{ 
-            mb: 2,
-            fontFamily: "'Open Sans', sans-serif",
-            textAlign:'justify'
-         }}
+      <Box
+        component="p"
+        sx={{
+          mb: 2,
+          fontFamily: "'Open Sans', sans-serif",
+          textAlign: "left",
+        }}
         data-aos="zoom-in-left" // Animación para este texto
       >
-        Ofrecemos un sistema con un costo inicial accesible para que tu negocio pueda comenzar a operar sin grandes inversiones. Esta implementación asegura que los emprendedores y empresas puedan tener una tienda online funcional sin preocupaciones financieras desde el primer día.
+        Ofrecemos un sistema con un costo inicial accesible para que tu negocio
+        pueda comenzar a operar sin grandes inversiones. Esta implementación
+        asegura que los emprendedores y empresas puedan tener una tienda online
+        funcional sin preocupaciones financieras desde el primer día.
       </Box>
-      <Divider sx={{ 
-        marginY: 2,
-        borderColor: '#65BDD2',
-        borderBottomWidth: 2 
-      }} />
-
-      {/* Sección 2 */}
-      <Box 
-        component="h6" 
+      <Divider
         sx={{
-            fontWeight: 'bold', mb: 1,
-            fontFamily: "'Raleway', sans-serif",
-            fontSize: '1.3rem'
-        }} 
-        data-aos="zoom-in-right" // Animación para esta sección
-      >
-        Sin Comisiones sobre Ventas
-      </Box>
-      <Box 
-        component="p" 
-        sx={{ 
-            mb: 2,
-            fontFamily: "'Open Sans', sans-serif",
-            textAlign:'justify'
-         }}
-        data-aos="zoom-in-right" // Animación para este texto
-      >
-        A diferencia de otras plataformas en el mercado, nuestra solución no retiene comisiones sobre tus ventas. Cada venta que realices es 100% para tu negocio, permitiéndote maximizar tus ingresos sin sorpresas a fin de mes.
-      </Box>
-      <Divider sx={{ 
-        marginY: 2,
-        borderColor: '#65BDD2',
-        borderBottomWidth: 2 
-      }} />
+          marginY: 2,
+          borderColor: "#65BDD2",
+          borderBottomWidth: 2,
+          marginTop: "5rem",
+          marginBottom: "5rem",
+          marginTop: "5rem",
+          marginBottom: "5rem",
+        }}
+      />
 
       {/* Sección 3 */}
-      <Box 
-        component="h6" 
+      <Box
+        component="h6"
         sx={{
-            fontWeight: 'bold', mb: 1,
-            fontFamily: "'Raleway', sans-serif",
-            fontSize: '1.3rem'
-        }} 
+          fontWeight: "bold",
+          mb: 1,
+          fontFamily: "'Raleway', sans-serif",
+          fontSize: "1.3rem",
+          margin: "1rem",
+          marginRight: "0rem",
+          textAlign: "right",
+        }}
         data-aos="zoom-in-left" // Animación para esta sección
       >
         Pago de Mantenimiento
       </Box>
-      <Box 
-        component="p" 
-        sx={{ 
-            mb: 2,
-            fontFamily: "'Open Sans', sans-serif",
-            textAlign:'justify'
+      <Box
+        component="p"
+        sx={{
+          mb: 2,
+          fontFamily: "'Open Sans', sans-serif",
+          textAlign: "right",
         }}
         data-aos="zoom-in-left" // Animación para este texto
       >
-        Para mantener tu sistema en óptimas condiciones y garantizar la disponibilidad, ofrecemos un mantenimiento continuo. Este costo se ajusta al uso del servidor y te asegura una plataforma siempre funcional y actualizada.
+        Para mantener tu sistema en óptimas condiciones y garantizar la
+        disponibilidad, ofrecemos un mantenimiento continuo. Este costo se
+        ajusta al uso del servidor y te asegura una plataforma siempre funcional
+        y actualizada.
       </Box>
-      <Divider sx={{ 
-        marginY: 2,
-        borderColor: '#65BDD2',
-        borderBottomWidth: 2 
-      }} />
+      <Divider
+        sx={{
+          marginY: 2,
+          borderColor: "#65BDD2",
+          borderBottomWidth: 2,
+          marginTop: "5rem",
+          marginBottom: "5rem",
+        }}
+      />
 
       {/* Sección 4 */}
-      <Box 
-        component="h6" 
+      <Box
+        component="h6"
         sx={{
-            fontWeight: 'bold', mb: 1,
-            fontFamily: "'Raleway', sans-serif",
-            fontSize: '1.3rem'
-        }} 
+          fontWeight: "bold",
+          mb: 1,
+          fontFamily: "'Raleway', sans-serif",
+          fontSize: "1.3rem",
+          margin: "1rem",
+          marginLeft: "0",
+          textAlign: "left",
+        }}
         data-aos="zoom-in-right" // Animación para esta sección
       >
         Competitividad en el Mercado
       </Box>
-      <Box 
-        component="p" 
-        sx={{ 
-            mb: 2,
-            fontFamily: "'Open Sans', sans-serif",
-            textAlign:'justify'
+      <Box
+        component="p"
+        sx={{
+          mb: 2,
+          fontFamily: "'Open Sans', sans-serif",
+          textAlign: "left",
         }}
         data-aos="zoom-in-right" // Animación para este texto
       >
-        Nuestro enfoque está diseñado para superar las barreras que otros proveedores imponen, como comisiones por ventas o altos costos iniciales. Nos comprometemos a ofrecer un servicio más justo y alineado con las necesidades de nuestros clientes.
+        Nuestro enfoque está diseñado para superar las barreras que otros
+        proveedores imponen, como comisiones por ventas o altos costos
+        iniciales. Nos comprometemos a ofrecer un servicio más justo y alineado
+        con las necesidades de nuestros clientes.
+      </Box>
+      <Divider
+        sx={{
+          marginY: 2,
+          borderColor: "#65BDD2",
+          borderBottomWidth: 2,
+          marginTop: "5rem",
+          marginBottom: "5rem",
+        }}
+      />
+      {/* Sección 2 */}
+      <Box
+        component="h6"
+        sx={{
+          fontWeight: "bold",
+          mb: 1,
+          fontFamily: "'Raleway', sans-serif",
+          fontSize: "1.3rem",
+          margin: "1rem",
+          marginRight: "0rem",
+          textAlign: "right",
+        }}
+        data-aos="zoom-in-right" // Animación para esta sección
+      >
+        Sin Comisiones sobre Ventas
+      </Box>
+      <Box
+        component="p"
+        sx={{
+          mb: 2,
+          fontFamily: "'Open Sans', sans-serif",
+          textAlign: "right",
+        }}
+        data-aos="zoom-in-right" // Animación para este texto
+      >
+        A diferencia de otras plataformas en el mercado, nuestra solución no
+        retiene comisiones sobre tus ventas. Cada venta que realices es 100%
+        para tu negocio, permitiéndote maximizar tus ingresos sin sorpresas a
+        fin de mes.
       </Box>
     </Box>
   );
